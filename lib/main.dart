6ce2f400f7a60b89da40692d2097977b827e1e12
parth_multipz <multipz.parth@gmail.com>
2021-07-14 18:16:17 +0530

    theme added by Parth Pitroda

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'utils/theme.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -10,10 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-
-        primarySwatch: Colors.blue,
-      ),
+      theme: darkTheme,
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
